6d95563eb0ac57e61e518793a16d5893df1dca02
Gabriel Ferreira <108829234+gabfsilva@users.noreply.github.com>
2023-03-17 21:04:43 -0300

    Teste PWA 5.1

--- service-worker.js
+++ service-worker.js
@@ -3,9 +3,34 @@ const CACHE_NAME = 'enxaguaki-cache-v1';
 
 // Lista de arquivos que serão armazenados em cache
 const urlsToCache = [
-  'index.html',
-  'assets/css/style-starter.css',
-  'assets/js/'
+  './index.html',
+  './assets/css/style-starter.css',
+  './assets/js/bootstrap.min',
+  './assets/js/counter.js',
+  './assets/js/jquery-3.3.1.min',
+  './assets/js/owl.carousel.js',
+  './assets/js/theme-change.js',
+  './assets/images/about1.jpg',
+  './assets/images/about2.jpg',
+  './assets/images/about3.jpg',
+  './assets/images/about4.jpg',
+  './assets/images/blog1.jpg',
+  './assets/images/blog2.jpg',
+  './assets/images/blog3.jpg',
+  './assets/images/inner-banner.jpg',
+  './assets/images/service1.jpg',
+  './assets/images/service2.jpg',
+  './assets/images/service3.jpg',
+  './assets/images/service4.jpg',
+  './assets/images/service5.jpg',
+  './assets/images/service6.jpg',
+  './assets/images/team1.jpg',
+  './assets/images/team2.jpg',
+  './assets/images/team3.jpg',
+  './assets/images/testi1.jpg',
+  './assets/images/testi2.jpg',
+  './assets/images/testi3.jpg',
+  './assets/images/testimonials.jpg'
 ];
 
 // Registra o Service Worker
